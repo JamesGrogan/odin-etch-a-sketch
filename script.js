0f64a7ddadd62f65aecb7a93430d689f1a7679f2
James Grogan <jgrogan@palantir.com>
2022-06-16 19:41:38 +0100

    Make squares coloured and better square-shapes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const mainContainer = document.querySelector('#main-container');
 for (let row = 1; row <= 16; row++) {
     const gridRow = document.createElement('div');
-    gridRow.classList.add('gridRow');
+    gridRow.classList.add('gridRowContainer');
     gridRow.classList.add(("row" + row));
     mainContainer.append(gridRow);
     for (let col = 1; col <= 16; col++) {
         const div = document.createElement('div');
         div.classList.add('tile');
-        div.textContent = col;
+        //col > 1 ? div.textContent = col : div.textContent = (`Row:${row} ${col}`);
         gridRow.append(div);
     }
 }
