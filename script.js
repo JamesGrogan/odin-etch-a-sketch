308065d86357d87b69cdefab3b72713c6b551628
James Grogan <jgrogan@palantir.com>
2022-06-16 20:22:17 +0100

    Generate new grid button functioning

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
 const mainContainer = document.querySelector('#main-container');
-for (let row = 1; row <= 16; row++) {
-    const gridRow = document.createElement('div');
-    gridRow.classList.add('gridRowContainer');
-    gridRow.classList.add(("row" + row));
-    mainContainer.append(gridRow);
-    for (let col = 1; col <= 16; col++) {
-        const div = document.createElement('div');
-        div.classList.add('tile');
-        //col > 1 ? div.textContent = col : div.textContent = (`Row:${row} ${col}`);
-        gridRow.append(div);
+
+function initialiseFirstGrid() {
+    generateGrid(16);
+    attachEventHandlers();
+}
+
+function generateGrid(size) {
+    for (let row = 1; row <= size; row++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('gridRowContainer');
+        gridRow.classList.add(("row" + row));
+        mainContainer.append(gridRow);
+        for (let col = 1; col <= size; col++) {
+            const div = document.createElement('div');
+            div.classList.add('tile');
+            //col > 1 ? div.textContent = col : div.textContent = (`Row:${row} ${col}`);
+            gridRow.append(div);
+        }
     }
 }
 
-const tiles = document.querySelectorAll('.tile');
-
-for (let i = 0; i < tiles.length; i ++) {
-    tiles[i].addEventListener('mouseover', () => {
+function attachEventHandlers() {
+    const tiles = document.querySelectorAll('.tile');
+    for (let i = 0; i < tiles.length; i ++) {
+        tiles[i].addEventListener('mouseover', () => {
         tiles[i].classList.add('hovered')
     });
+    }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function destroyGrid() {
+    removeAllChildNodes(mainContainer);
 }
 
 resetButton = document.querySelector('#reset-button');
 console.log(resetButton);
 resetButton.addEventListener('click', () => {
-    response = prompt('Please enter the size of the new grid');
-    console.log(response);
+    const size = prompt('Please enter the size of the new grid');
+    destroyGrid();
+    generateGrid(size);
+    attachEventHandlers();
 })
